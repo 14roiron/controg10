a9cb067f96bcec6547a31dbd8fc936e609cf315a
14roiron <14roiron@users.noreply.github.com>
2015-05-28 10:03:11 +0200

    Create bibliographie.php

---
+++ bibliographie.php
@@ -0,0 +1,54 @@
+<?php
+include("inc/header.php");
+?>
+
+<!--main-->
+<div class="container">
+	<div class="row">
+      <!--left-->
+      <div class="col-md-3" id="leftCol">
+        <ul class="nav nav-stacked well well-lg" id="sidebar">
+        </ul>
+      </div><!--/left-->
+      
+      <!--right-->
+      <div class="col-md-9">
+        <div class="paragraphe">
+        <h2 id="sw">Sites Webs</h2>
+        <p>
+          At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
+          find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.</p>
+        
+        <hr> </div>
+       
+       
+    <div class="paragraphe">
+        <h2 id="Journaux">Journaux</h2>
+        <p>
+          At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
+          find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.</p>
+        
+        <hr> </div>
+        
+        
+        
+         <div class="paragraphe">
+        <h2 id="pb">Publications</h2>
+        <p>
+          At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
+          find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.</p>
+        
+        <hr> </div>
+        
+        </div><!--/right-->
+  	</div><!--/row-->
+</div><!--/container-->
+
+<?php
+include("inc/footer.php");
+?>
+<script language="javascript">
+//$("#listeHeader1").addClass("active");
+</script>
+	</body>
+</html>
