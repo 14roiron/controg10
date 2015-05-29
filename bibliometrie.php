964c0ff1e0ce89112130d54d5cde6a845c47a76b
14chanwa <quentin.cwn@gmail.com>
2015-05-29 22:02:20 +0200

    Ajout d'une partie sur altmetrics

--- bibliometrie.php
+++ bibliometrie.php
@@ -301,6 +301,55 @@ Chaque indicateur bibliométrique est sujet à des débats de ce type. Chaque in
 Il s'agit donc de contextualiser, c'est-à-dire de ne pas se borner à regarder un seul indicateur bibliométrique afin de chercher à évaluer un chercheur ou d'adapter les indicateurs en fonction des domaines concernés.
 </p>
 
+<div class="altmetricsma">
+<h2 id="titre">Altmetrics, les mesures alternatives aux indicateurs bibliométriques</h2>
+
+<p>
+>>>>>>>>>>>>>>>>IMAGE altmetrics_logo.png<<<<<<<<<<<<<<<<<<<<<<
+</p>
+
+<p>
+Parallèlement aux indicateurs bibliométriques, qui utilisent principalement le lien citationnel, il est également utilisé d'autres indicateurs se basant sur des données très différentes. En 2010, on entend pour la première fois le terme de "altmetrics", qui désigne des mesures dites "non traditionnelles", telles que décrites dans le manifeste de altmetrics.
+</p>
+
+	<blockquote>
+	No one can read everything.  We rely on filters to make sense of the scholarly literature, but the narrow, traditional filters are being swamped. However, the growth of new, online scholarly tools allows us to make new filters; these altmetrics reflect the broad, rapid impact of scholarship in this burgeoning ecosystem. We call for more tools and research based on altmetrics. (...)
+	In growing numbers, scholars are moving their everyday work to the web. Online reference managers Zotero and Mendeley each claim to store over 40 million articles (making them substantially larger than PubMed); as many as a third of scholars are on Twitter, and a growing number tend scholarly blogs.
+	<small class="pull-right">J. Priem, D. Taraborelli, P. Groth, C. Neylon, <a href="http://altmetrics.org/">altmetrics</a>	: a manifesto, 2010</small><br>
+	  </blockquote> 
+
+<p>
+Ces mesures viendraient donc "alternativement" aux mesures utilisées pour calculer les indicateurs bibliométriques. Elles concernent les différents articles qui sont disponibles, non pas dans les bases de données mais sur internet. Les éléments mesurés sont, pour une certaine production scientifique, par exemple, des indicateurs liés à la popularité sur les réseaux sociaux : nombre de vues, nombre de téléchargements... Ainsi, altmetrics prendrait en compte une quantité plus importante d'articles.
+</p>
+
+<p>
+L'utilisation des indicateurs sus-cités est d'une philosophie fondamentalement différente de celle des indicateurs bibliométriques. En effet, il s'agit ici de mesurer quelque chose d'autre, qui n'est pas forcément mis en exergue par des indicateurs bibliométriques basés sur le lien citationnel. Par exemple, Michel Zitt, chercheur à l'INRA, mentionne la popularité d'une entité sur le web :
+</p>
+
+	<blockquote>
+	Il y a l’idée de mesurer de nouveaux objets, de nouvelles choses, par exemple la présence des différentes universités sur le net, c’est à dire le degré d’activité qu’elles ont sur la toile, qui est donc une notion de communication en quelque sorte, une partie managériale, l’efficacité de la communication de ces universités. 
+	<small class="pull-right">Michel Zitt, INRA, Entretien</small><br>
+	  </blockquote> 
+  
+<p>
+Les créateurs de la startup altmetrics voient ces mesures comme complémentaires des mesures basées sur le lien citationnel. En particulier, ils ne souffriraient pas des mêmes "défauts" que ces dernières, notamment leur inertie, et le fait qu'ils ne prennent pas en compte l'influence des travaux en dehors du milieu académique. J. Priem, membre fondateur de altmetrics, mentionne en entretien :
+</p>
+
+	<blockquote>
+	Any kind of citation mining, whether from the ISI, WoS or from Scopus, is gonna be limited by the fact that citation has certain weakness associated to them. For instance, a citation will take a certain amount of time to show up: the word has to be published, someone has to read it, they have to get their own republish... So often there is a delay, as much as several years, between one of our creative impact and when we can observe that impact in the citation record.
+	<small class="pull-right">J. Priem, <a href="http://altmetrics.org/">altmetrics</a>, Entretien</small><br>
+	  </blockquote> 
+
+<p>
+Ces mesures alternatives seraient donc plus actuelles que les mesures liées à la citation, car plus rapide et touchant un nombre plus important d'acteurs de la recherche.
+</p>
+
+<div class="selectivitearticles">
+<h2 id="titre">Sélectivité des articles : quantité et qualité</h2>
+
+<p>
+A COMPLETER
+</p>
 
 
 <hr>
