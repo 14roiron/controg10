f29547e186fde553c98b73a6a01b2f9404a40db2
14roiron <yohann.roiron@mines-paristech.fr>
2015-05-29 17:33:01 +0200

    correction linkage auto
    correction erreur js

--- inc/footer.php
+++ inc/footer.php
@@ -23,6 +23,7 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/scripts.js"></script>
+		<script src="js/fncts.js"></script>
 		<script>//javascript google analytic
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
