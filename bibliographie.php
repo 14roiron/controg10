8665656fd88b513bbb66685419d8d8fc251d0e82
14roiron <yohann.roiron@mines-paristech.fr>
2015-05-30 19:44:50 +0200

    ajout lib

--- bibliographie.php
+++ bibliographie.php
@@ -24,10 +24,46 @@ include("inc/header.php");
        
     <div class="paragraphe">
         <h2 id="Journaux">Journaux</h2>
-        <p>
-          At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
-          find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.</p>
         
+<p>
+[1] Lorna Wildgaard, Jesper W. Schneider, Birger Larsen. “A review of the characteristics of 108 author-level bibliometric indicators” (2014),Scientometrics 101:125–158
+</p>
+<p>
+[2] J. Hirsh, (2005). An index to quantify an individual's scientific research output. Proceedings of the National Academy of Sciences, vol. 102, no 46,‎ 15 nov. 2005, 16569–16572.
+</p>
+<p>
+[3] L. Egghe, (2006). Theory and practise of the g-index. Scientometrics, vol. 69, No 1, 131-152.
+</p>
+<p>
+[4] G. Abramo, D'Angelo, C., & Viel, F. (2013). The suitability of h and g indexes for measuring the research performance of institutions.<i> Scientometrics 97 </i>, 555–570.
+</p>
+<p>
+[5] E. Garfield,  "Journal impact factor: a brief review", <i> Canadian Medical Association Journal </i>, 161(8), p.979-80, 1999
+</p>
+<p>
+[6] J. K. Vanclay Impact factor: outdated artefact or stepping-stone to journal certification?Scientometrics 2011, 24 nov
+Michel Zitt, The journal impact factor: angel, devil, or scapegoat? A comment on J.K. Vanclay’s article 2011, <i> Scientometrics </i> 2012, 22 feb
+</p>
+<p>
+[7] Jevin D. West, Michael C. Jensen, Ralph J. Dandrea, Gregory J. Gordon, Carl T. Bergstrom, “Author-Level Eigenfactor Metrics: Evaluating the Influence of Authors, Institutions, and Countries within the Social Science Research Network Community”, <i> Journal of the American Society for Information Science and Technology </i>, 64(4):787–801, 2013
+</p>
+<p>
+[8] Liu, J, and Adie, E, “New perspectives on article-level metrics: developing ways to assess research uptake and impact online”, <i>Insights </i>, 2013 , 27(2), 153–158 
+</p>
+<p>
+[9] J.Gorraiz, P.J. Purnell, W. Gländel, Opportunities for and Limitations of the Book Citation Index,<i> Journal of the American society for information science and technology </i>, 64(7) :1388-1398,  July 2013
+</p>
+<p>
+[10] J. Gorriaz, C. Gumpenberger, P.J. Purnell , The power of book reviews : a simple and transparent enhancement approach for book citation indexes, <i>Scientometrics </i> (2014) 98:841–852
+</p>
+<p>
+[11] K. Kousha, M. Thelwall, S. Rezaie, Assessing the Citaion Impact of Books : The Role of Google Books, Google Scholar, and Scopus, <i>Journal of the American society for information science and technology </i>, 62(11) :2147-2164, 2011
+</p>
+<p>
+[12] K. Kousha, M. Thelwall. Sources of Google Scholar citations outside the Science Citation Index: A comparison between four science disciplines,<i> Scientometrics </i>, Vol. 74, No. 2 (2008) 273–294
+</p>
+
+[13] P. Jasco. Grim tales about the impact factor and the h-index in the Web of Science and the Journal Citation Reports databases: reflections on Vanclay’s criticism, <i> Scientometrics </i> , 2012, 19 avril
         <hr> </div>
         
         
@@ -49,6 +85,7 @@ include("inc/footer.php");
 ?>
 <script language="javascript">
 $("#bibliographie").addClass("active");
+parse();
 </script>
 	</body>
 </html>
