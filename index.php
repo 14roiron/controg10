7efbcd7345d2fc9be441c51e9e4f4022247f707e
14chanwa <quentin.cwn@gmail.com>
2015-05-31 14:34:49 +0200

    Ajouts

--- index.php
+++ index.php
@@ -36,6 +36,14 @@ include("inc/header.php");
 				<li>Divers entretiens réalisés avec les acteurs de la controverse</li>
 		  </ul>
         
+        <p>
+        Le monde de la recherche comporte de très nombreux acteurs. Le principal moyen de communication entre ces différents acteurs consiste à partager les différentes avancées par le biais de différentes productions scientifiques : conférences, articles, livres, etc. Au delà du simple partage d'informations, des liens complexes se tissent entre les différents acteurs de la recherche.
+        </p>
+        
+        <p>
+        Par exemple, un problème récurrent à propos de la recherche consiste en les modalités de son évaluation. Comment repérer parmi les milliers de publications disponibles celles qui apportent des résultats hors du commun, comment au contraire tenter de se faire lire, telles sont les problématiques du point de vue du chercheur. Comment attribuer les crédits de recherche, et à qui, comment évaluer la performance et l’impact d’un chercheur ou d’un centre de recherche, c’est la problématique du point de vue des institutions et des ministères. Comment comparer une université à une autre, un pays à un autre en fonction de la puissance de sa recherche, c’est l’objectif des classements internationaux. Les débats se multiplient à propos de la condition du chercheur, ou de la publication de  classements aux enjeux de plus en plus importants, dans la sphère publique comme parmi la communauté scientifique. L’évaluation de la recherche se fait bien souvent par le biais de la bibliométrie. Bibliométrie, scientométrie sont des domaines à part entière de la recherche scientifique, visant à déterminer les indicateurs pertinents à tel ou tel usage, et selon telles ou telles situations. La recherche d’un indicateur bibliométrique à la fois simple à déterminer et donnant des résultats pertinents est très active ; existe-t-il, ou doit-on en combiner plusieurs avec leurs défauts et avantages, et lesquels ? Cette question est centrale dans la controverse et les désaccords se multiplient selon les intérêts de chacun des acteurs.
+        </p>
+        
         </div>
 
         </div><!--/right-->
