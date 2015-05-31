04959b3a7b1a1b16bc43dec330890d533cfe731f
14chanwa <quentin.cwn@gmail.com>
2015-05-31 15:27:50 +0200

    Corrections

--- bibliographie.php
+++ bibliographie.php
@@ -39,58 +39,58 @@ include("inc/header.php");
 
 
 <p>
-G. Abramo, D'Angelo, C., & Viel, F. (2013). The suitability of h and g indexes for measuring the research performance of institutions.<i> Scientometrics 97 </i>, 555–570.
+G. Abramo, C. D'Angelo, F. Viel, The suitability of h and g indexes for measuring the research performance of institutions.<i> Scientometrics</i>,Volume 97 , 555–570, 2013.
 </p>
 <p>
-L. Egghe. Theory and practise of the g-index. <i>Scientometrics</i>, vol. 69, No 1, 131-152, 2006
+L. Egghe, Theory and practise of the g-index. <i>Scientometrics</i>, vol. 69, No 1, 131-152, 2006.
 </p>
 <p>
-E. Garfield.  Journal impact factor: a brief review, <i> Canadian Medical Association Journal </i>, 161(8), p.979-80, 1999
+E. Garfield,  Journal impact factor: a brief review, <i> Canadian Medical Association Journal </i>, 161(8), p.979-80, 1999.
 </p>
 <p>
-J. Gorraiz, P.J. Purnell, W. Gländel, Opportunities for and Limitations of the Book Citation Index,<i> Journal of the American society for information science and technology </i>, 64(7) :1388-1398,  Juillet 2013
+J. Gorraiz, P.J. Purnell, W. Gländel, Opportunities for and Limitations of the Book Citation Index,<i> Journal of the American society for information science and technology </i>, 64(7) :1388-1398,  Juillet 2013.
 </p>
 <p>
-J. Gorriaz, C. Gumpenberger, P.J. Purnell , The power of book reviews : a simple and transparent enhancement approach for book citation indexes, <i>Scientometrics </i> 98:841–852, 2014
+J. Gorriaz, C. Gumpenberger, P.J. Purnell, The power of book reviews : a simple and transparent enhancement approach for book citation indexes, <i>Scientometrics </i> 98:841–852, 2014.
 </p>
 <p>
-J. Hirsh. An index to quantify an individual's scientific research output. <i>Proceedings of the National Academy of Sciences</i>, vol. 102, no 46, 16569–16572, 15 Novembre 2005
+J. Hirsh, An index to quantify an individual's scientific research output. <i>Proceedings of the National Academy of Sciences</i>, vol. 102, no 46, 16569–16572, 15 Novembre 2005.
 </p>
 <p>
-P. Jacso. Grim tales about the impact factor and the h-index in the Web of Science and the Journal Citation Reports databases: reflections on Vanclay’s criticism, <i> Scientometrics </i> , 19 Avril 2012
+P. Jacso, Grim tales about the impact factor and the h-index in the Web of Science and the Journal Citation Reports databases: reflections on Vanclay’s criticism, <i> Scientometrics </i> , 19 Avril 2012.
 </p>
 <p>
-K. Kousha, M. Thelwall, S. Rezaie, Assessing the Citaion Impact of Books : The Role of Google Books, Google Scholar, and Scopus, <i>Journal of the American society for information science and technology </i>, 62(11) :2147-2164, 2011
+K. Kousha, M. Thelwall, S. Rezaie, Assessing the Citaion Impact of Books : The Role of Google Books, Google Scholar, and Scopus, <i>Journal of the American society for information science and technology </i>, 62(11) :2147-2164, 2011.
 </p>
 <p>
-K. Kousha, M. Thelwall. Sources of Google Scholar citations outside the Science Citation Index: A comparison between four science disciplines,<i> Scientometrics </i>, Vol. 74, No. 2 273–294, 2008
+K. Kousha, M. Thelwall, Sources of Google Scholar citations outside the Science Citation Index: A comparison between four science disciplines,<i> Scientometrics </i>, Vol. 74, No. 2 273–294, 2008.
 </p>
 <p>
-J. Liu, E. Adie, “New perspectives on article-level metrics: developing ways to assess research uptake and impact online”, <i>Insights </i>, 27(2), 153–158, 2013
+J. Liu, E. Adie, New perspectives on article-level metrics: developing ways to assess research uptake and impact online, <i>Insights </i>, 27(2), 153–158, 2013.
 </p>
 <p>
-J. Priem, D. Taraborelli, P. Groth, C. Neylon, altmetrics: a manifesto, 2010
+J. Priem, D. Taraborelli, P. Groth, C. Neylon, altmetrics: a manifesto, 2010.
 </p>
 <p>
-J. K. Vanclay, An evaluation of the Australian Research Council's journal ranking, <i>Scientometrics</i> Volume 5, Issue 2, Pages 265–274, Avril 2011
+J. K. Vanclay, An evaluation of the Australian Research Council's journal ranking, <i>Scientometrics</i> Volume 5, Issue 2, Pages 265–274, Avril 2011.
 </p>
 <p>
-J. K. Vanclay, L. Bornmann, Metrics to evaluate research performance in academic institutions: a critique of ERA 2010 as applied in forestry and the indirect H2 index as a possible alternative, <i>Scientometrics</i>, Volume 91, Issue 3, pp 751-771, Juin 2012
+J. K. Vanclay, L. Bornmann, Metrics to evaluate research performance in academic institutions: a critique of ERA 2010 as applied in forestry and the indirect H2 index as a possible alternative, <i>Scientometrics</i>, Volume 91, Issue 3, pp 751-771, Juin 2012.
 </p>
 <p>
-J. K. Vanclay, Impact factor: outdated artefact or stepping-stone to journal certification?, <i>Scientometrics</i>, Volume 92, Issue 2, pp 211-238, Août 2012
+J. K. Vanclay, Impact factor: outdated artefact or stepping-stone to journal certification?, <i>Scientometrics</i>, Volume 92, Issue 2, pp 211-238, Août 2012.
 </p>
 <p>
-J.D. West, M.C. Jensen, R.J. Dandrea, G.J. Gordon, C.T. Bergstrom, Author-Level Eigenfactor Metrics: Evaluating the Influence of Authors, Institutions, and Countries within the Social Science Research Network Community, <i> Journal of the American Society for Information Science and Technology </i>, 64(4):787–801, 2013
+J.D. West, M.C. Jensen, R.J. Dandrea, G.J. Gordon, C.T. Bergstrom, Author-Level Eigenfactor Metrics: Evaluating the Influence of Authors, Institutions, and Countries within the Social Science Research Network Community, <i> Journal of the American Society for Information Science and Technology </i>, 64(4):787–801, 2013.
 </p>
 <p>
-Lorna Wildgaard, Jesper W. Schneider, Birger Larsen. A review of the characteristics of 108 author-level bibliometric indicators, <i>Scientometrics</i>, 101:125–158, 2014
+Lorna Wildgaard, Jesper W. Schneider, Birger Larsen, A review of the characteristics of 108 author-level bibliometric indicators, <i>Scientometrics</i>, 101:125–158, 2014.
 </p>
 <p>
-M. Zitt, The journal impact factor: angel, devil, or scapegoat? A comment on J.K. Vanclay’s article, <i> Scientometrics </i>, 22 Février 2012
+M. Zitt, The journal impact factor: angel, devil, or scapegoat? A comment on J.K. Vanclay’s article, <i> Scientometrics </i>, 22 Février 2012.
 </p>
 <p>
-San Francisco Declaration on Research Assessment ; <a href="http://www.ascb.org/dora-old/files/SFDeclarationFINAL.pdf">DORA</a>, 2013
+San Francisco Declaration on Research Assessment ; <a href="http://www.ascb.org/dora-old/files/SFDeclarationFINAL.pdf">DORA</a>, 2013.
 </p>
 
         <hr> </div>
@@ -99,10 +99,10 @@ San Francisco Declaration on Research Assessment ; <a href="http://www.ascb.org/
         <div class="paragraphe">
         <h2 id="presse">Presse</h2>
         <p>
-        J. Anquetin, O. Béthoux, P-O. Antoine, A. Boura, J. Martin, D. Germain, R. Allain, L. Hautier, R. Thomas, D. de Franceschi. « Intégrité et recherche : il faut revoir les critères d'évaluation ». Le Monde.fr. 16 Décembre 2014.
+        J. Anquetin, O. Béthoux, P-O. Antoine, A. Boura, J. Martin, D. Germain, R. Allain, L. Hautier, R. Thomas, D. de Franceschi. « Intégrité et recherche : il faut revoir les critères d'évaluation ». Le Monde.fr, 16 Décembre 2014.
         </p>
         <p>
-        L. Bronner, « Recherche : les universités françaises réalisent des performances mitigées ». Le monde, 23 Août 2005. 
+        L. Bronner, « Recherche : les universités françaises réalisent des performances mitigées ». Le Monde, 23 Août 2005. 
         </p>
         <p>
         B. Cronin, « Comment la manie des rankings a atteint le cœur de la recherche ». Le Temps, 24 Octobre 2014.
@@ -111,25 +111,25 @@ San Francisco Declaration on Research Assessment ; <a href="http://www.ascb.org/
         J. Elayi, « L’évaluation des chercheurs en question ». Le Figaro, 27 Décembre 2004.
         </p>
         <p>
-        Y. Gingras, « Le Classement de Shanghai n’est pas scientifique ». La Recherche, no. 430, Mai 2009
+        Y. Gingras, « Le Classement de Shanghai n’est pas scientifique ». La Recherche, no. 430, Mai 2009.
         </p>
         <p>
-        S. Huet,  « Les chercheurs en quête d'argent et d'idées », Libération, 30 Juin 2003
+        S. Huet,  « Les chercheurs en quête d'argent et d'idées », Libération, 30 Juin 2003.
         </p>
         <p>
-        M. Leduc, S. Jaffard, J-C. Bernier, « La bonne méthode d’évaluation des chercheurs », Les Echos, 23 Juillet 2009
+        M. Leduc, S. Jaffard, J-C. Bernier, « La bonne méthode d’évaluation des chercheurs », Les Echos, 23 Juillet 2009.
         </p>
         <p>
         D. Raoult, « L'éruption de l'économie de marché dans la mesure de l'activité scientifique », 9 Avril 1998.
         </p>
         <p>
-        M. Zito, « Les dégâts collatéraux du facteur d’impact ». Le Monde. 16 Février 2013.
+        M. Zito, « Les dégâts collatéraux du facteur d’impact ». Le Monde, 16 Février 2013.
         </p>
         <p>
-        « Connaître n’est pas mesurer ». Le Monde,  Science & techno, 29 Janvier 2014.
+        « Connaître n’est pas mesurer ». Le Monde, 29 Janvier 2014.
         </p>
         <p>
-        « Les programmes de recherche européens en quête de simplicité et d'efficacité », Les Echos, SCIENCES, 16 Novembre 2000.
+        « Les programmes de recherche européens en quête de simplicité et d'efficacité », Les Echos, 16 Novembre 2000.
         </p>
 
         <hr> </div>
